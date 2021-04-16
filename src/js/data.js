a386143dc4f524cab93903dd91cb3269d65068c5
cass1618 <cass16180@gmail.com>
2021-04-16 14:26:17 -0700

    Add assignId method to Data class, pass test

--- src/js/data.js
+++ src/js/data.js
@@ -1,12 +1,13 @@
 export default class Data {
   constructor() {
   this.lines = {};
+  this.currentId = 0;
   }
 
-  //assignId() {
-    // this.currentId++;
-    // return this.currentId;
-  //}
+  assignId() {
+    this.currentId++;
+    return this.currentId;
+  }
 
   //addLine(line) {
     // this.id = this.assignId();
