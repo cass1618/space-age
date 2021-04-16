b63feb41735f26dea491eb44a3234989a45398f9
cass1618 <cass16180@gmail.com>
2021-04-16 15:07:55 -0700

    Define addLine method for Data object, pass test

--- src/js/data.js
+++ src/js/data.js
@@ -9,8 +9,26 @@ export default class Data {
     return this.currentId;
   }
 
-  //addLine(line) {
-    // this.id = this.assignId();
-    // this.lines[line.id] 
-  //}
+  addLine(line) {
+    line.id = this.assignId();
+    this.lines[line.id] = line;
+  }
 }
+
+// function Data() {
+//   this.lines = {};
+//   this.currentId = 0;
+//   }
+
+
+//   Data.prototype.assignId = function() {
+//     this.currentId++;
+//     return this.currentId;
+//   }
+//   Data.prototype.addLine = function(line) {
+//     line.id = this.assignId();
+//     this.lines[line.id] = line;
+//   }
+
+//   let data = new Data();
+
