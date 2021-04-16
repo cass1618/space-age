0bb9d4d69af6111131bf103f51e079768bbc327e
cass1618 <cass16180@gmail.com>
2021-04-16 09:13:47 -0700

    Pass test for Person constructor

--- __tests__/person.js
+++ __tests__/person.js
@@ -1,6 +1,7 @@
 import Person from "../src/js/person.js";
 describe("Person", () => {
-  test("should correctly create a Person object with name and age", () => {
+
+  test("should correctly create a Person object with name", () => {
     const person = new Person("name",);
     expect(person);
     expect(person.name).toMatch("name");
---
+++ src/js/person.js
@@ -0,0 +1,5 @@
+export default class Pesron {
+  constructor(name) {
+    this.name = name;
+}
+}
