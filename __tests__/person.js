bee6ce5a8d44d52f1aa30f44fa153ab1520a3898
cass1618 <cass16180@gmail.com>
2021-04-16 09:34:34 -0700

    Update Person to include ages, good fail

--- __tests__/person.js
+++ __tests__/person.js
@@ -9,5 +9,9 @@ describe("Person", () => {
   test("should correctly create a Person object with name", () => {
     expect(person);
     expect(person.name).toMatch("Name");
+    expect(person.mercuryAge).toEqual(0);
+    expect(person.venusAge).toEqual(0);
+    expect(person.marsAge).toEqual(0);
+    expect(person.jupiterAge).toEqual(0);
   });
 });
