3934626261b6a228007d2e93edf7f317444d0a05
cass1618 <cass16180@gmail.com>
2021-04-16 09:11:54 -0700

    Add test for Person constructor, good fail

--- __tests__/person.js
+++ __tests__/person.js
@@ -1,8 +1,8 @@
-import classObj from "../src/js/person.js";
-describe("classObj", () => {
-  test("should correctly create an object", () => {
-    const object = new classObj("name");
-    expect(object);
-    expect(object.name).toMatch("name");
+import Person from "../src/js/person.js";
+describe("Person", () => {
+  test("should correctly create a Person object with name and age", () => {
+    const person = new Person("name",);
+    expect(person);
+    expect(person.name).toMatch("name");
   });
 });
