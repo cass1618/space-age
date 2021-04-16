78dec9920a55bedca722f1fd21c90a19224ff051
cass1618 <cass16180@gmail.com>
2021-04-16 15:06:32 -0700

    Add test for Data addLine method, good fail

--- __tests__/data.test.js
+++ __tests__/data.test.js
@@ -1,10 +1,13 @@
 import Data from "../src/js/data.js";
+import Line from "../src/js/line.js";
 
 describe("Data", () => {
   let data = {};
+  let line;
 
   beforeEach(() => {
     data = new Data();
+    line = new Line(50,50,50,50,50,50,50,50,50,50,50,50,50);
   });
   test("should correctly create an empty Data object", () => {
     expect(data);
@@ -12,7 +15,12 @@ describe("Data", () => {
     expect(data.currentId).toEqual(0);
   });
 
-  test("should correctly assign an id", () => {
+  test("should correctly return an id", () => {
     expect(data.assignId()).toEqual(1);
   });
+
+  test("should correctly add line to Data object", () => {
+    expect(data.addLine(line));
+      expect(data.lines).toEqual({'1': line});
+  }); 
 });
