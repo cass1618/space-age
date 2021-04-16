572ba080c0b24d7acd0bf10e80eb695a7f10ecdf
cass1618 <cass16180@gmail.com>
2021-04-16 10:21:31 -0700

    Add calculateMercuryAge function, pass

--- src/js/person.js
+++ src/js/person.js
@@ -1,9 +1,17 @@
 export default class Pesron {
-  constructor(name) {
+  constructor(name, age) {
     this.name = name;
+    this.earthAge = age;
     this.mercuryAge = 0;
     this.venusAge = 0;
     this.marsAge = 0;
     this.jupiterAge = 0;
-}
+  }
+
+  calculateMercuryAge() {
+    const mercuryAge = this.earthAge * .24;
+    this.mercuryAge = mercuryAge;
+    return mercuryAge;
+  }
+
 }
